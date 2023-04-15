4ff09cb4b6f5cc0770488891063f1a5b55f753a7
eduardsdn <eduard.sdn@gmail.com>
2023-04-15 18:06:47 +0200

    Adjusted advertiser tablet layout + fixed buttons on desktop webmaster
    (cherry picked from commit 9f420f18028d40efb91958272a0a42f7b6d2ac0d)
    
    Adjusted mobile webmaster layout
    
    (cherry picked from commit ed7db283d080c4d28c3b5333aff6940b0e114026)
    
    Modified webmaster tablet layout
    
    (cherry picked from commit f271f20f63e209a7fc1c6157b9fed5843f444385)
    
    added links for review cards
    
    (cherry picked from commit b94324ec951dc2857b4d15eab264727751c32ad0)
    
    Minor changes to desktop layout
    
    (cherry picked from commit 5dba95a0c0eb1dce715e7705e8c460d7d4d326cf)
    
    Added README
    
    (cherry picked from commit de0b93b78a5e4e7867ed274bfe71c1bfe48152d2)
    
    renamed links (/)2
    
    (cherry picked from commit d6258ee554a8e9d5435c75d3ed23b082a310300e)
    
    renamed links (/)
    
    (cherry picked from commit a57deb9bdfd7fdca3382a9ff1982af344a418a8e)
    
    Deleted media.css
    
    (cherry picked from commit abbbf9798e2f9108074b200d2f61c388894097d8)
    
    testing media queries
    
    (cherry picked from commit a389d4c48917f08361c468e42d57990bba96ae2b)
    
    Delete CNAME
    
    (cherry picked from commit 4121dcf00e1a26fe4692446a3b07b82531c764c9)
    
    Update CNAME
    
    (cherry picked from commit 7e8fd7099da865ec20eda8d9726b12e6144e3cff)
    
    Update CNAME
    
    (cherry picked from commit f7ca52f655ba6a58fb584062c8388587ce4513c2)
    
    Update CNAME
    
    (cherry picked from commit 3124b0e3d49b91ad4d22956a0f682874a8bea8a7)
    
    Create CNAME
    
    (cherry picked from commit 68e12b5c397cb9ecb4ff7ed5f8688c245508879a)
    
    Sliders are now draggable
    
    (cherry picked from commit 9604678e4f41b69a272d925aca9ccbb8a4a0fcd5)
    
    Styled mobile webmaster
    
    (cherry picked from commit 617425fa05583b95114529c8467e839a9e3c22e1)
    
    Initial stylying for mobile webmaster
    
    (cherry picked from commit 5c8670b379dbcdacd8da477398cae584fc83d05f)
    
    Stylefd tablet webmaster
    
    (cherry picked from commit cf3bfcb49da76313e58b794a8a8ecb03b858b520)
    
    Continued styling tablet webmaster
    
    (cherry picked from commit 1e74feb36fccd6368b9d6bb1a31fd954400c8c82)
    
    Styled tablet webmaster (still needs some work)
    
    (cherry picked from commit 932381cc9ca1668ea98155becc69619e6f12b28e)
    
    Styled mobile advertiser
    
    (cherry picked from commit b536e1d11b6103b4d169391e1edc81896ac8afd8)
    
    Styled tablet platforms section for advertiser + fixed logo shrink
    
    (cherry picked from commit f24094f2f0fa021fc23f938840e4bff573b1a7e5)
    
    styled tablet view for advertiser page(except for platforms)
    
    (cherry picked from commit 6762575aed2d0cb5fb160f53df3f3815a59117f6)
    
    Styled decor for advertiser page
    
    (cherry picked from commit b68299a91aaca41151a38b400ef401a3a7573a31)
    
    Styled decor elements
    
    (cherry picked from commit 01c3f89b289abf0f9f88efea73c3ee98fb09b0f6)
    
    Styled footer
    
    (cherry picked from commit 24c805ec6546f0b5b5535d068a9788d068bc69c0)
    
    Styled other-cases + set background img
    
    (cherry picked from commit b61a99e4d8e8e11950b89e0613f915cb6ac53347)
    
    Styled other-cases-card
    
    (cherry picked from commit 29d40d4c5bce23176d1adae095734b9702ee3535)
    
    Case section styled
    
    (cherry picked from commit 2a54875bf7d7c03fde587c7da46ca0d0d089091e)
    
    Platforms section
    
    (cherry picked from commit be5e990203eabd837ddace52f3df84fc52028c5f)
    
    Styled description and brands sections
    
    (cherry picked from commit 63c8f6240c9db18e6d01e187d67eb377e1c52bfa)
    
    Styled steps section
    
    (cherry picked from commit 87cd11a812dd410a8ad30328ead224b6331dcc2a)
    
    Styled header
    
    (cherry picked from commit cce1729c31d49967dbaf6ba25255cf4e7045d951)

---
+++ main.js
@@ -0,0 +1,112 @@
+function otherCasesSliderClick() {
+    const otherCasesSlider = document.querySelector(".other-cases-cards-slider")
+    const otherCasesPrevBtn = document.querySelector("#other-cases-next-button")
+    const otherCasesNextBtn = document.querySelector('#other-cases-prev-button')
+    
+    const otherCasesFirstCard = document.querySelectorAll(".other-cases-card")[0]
+    let otherCasesFirstCardWidth = otherCasesFirstCard.clientWidth
+
+    otherCasesNextBtn.addEventListener('click', function(){
+        otherCasesSlider.scrollLeft -=  otherCasesFirstCardWidth
+    })
+
+    otherCasesPrevBtn.addEventListener('click', function(){
+        otherCasesSlider.scrollLeft +=  otherCasesFirstCardWidth
+    })
+
+}
+
+function reviewsSliderClick() {
+    const reviewsSlider = document.querySelector(".reviews-cards-slider")
+    const reviewsPrevBtn = document.querySelector("#reviews-next-button")
+    const reviewsNextBtn = document.querySelector('#reviews-prev-button')
+
+    const reviewsFirstCard = document.querySelectorAll(".reviews-card ")[0]
+    let reviewsFirstCardWidth = reviewsFirstCard.clientWidth
+
+    reviewsNextBtn.addEventListener('click', function(){
+        reviewsSlider.scrollLeft -=  reviewsFirstCardWidth
+    })
+
+    reviewsPrevBtn.addEventListener('click', function(){
+        reviewsSlider.scrollLeft +=  reviewsFirstCardWidth
+    })
+}
+
+
+function otherCasesSliderDrag(){
+    const otherCasesSlider = document.querySelector(".other-cases-cards-slider")
+
+    let isDragStart = false, prevPageX, prevScrollLeft
+
+    const dragStart = (e) => {
+        isDragStart = true
+        prevPageX = e.pageX || e.touches[0].pageX
+        prevScrollLeft = otherCasesSlider.scrollLeft
+        console.log("mousedown")
+    }
+
+    const dragging = (e) => {
+        if(!isDragStart) return
+        e.preventDefault()
+        let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX
+        otherCasesSlider.scrollLeft = prevScrollLeft - positionDiff
+        console.log("dragging")
+    }
+
+    const dragStop = () => {
+        isDragStart = false
+        console.log("mouseeup")
+    }
+
+    otherCasesSlider.addEventListener('mousedown', dragStart)
+    otherCasesSlider.addEventListener('touchstart', dragStart)
+
+    otherCasesSlider.addEventListener('mousemove', dragging)
+    otherCasesSlider.addEventListener('touchmove', dragging)
+
+    otherCasesSlider.addEventListener('mouseup', dragStop)
+    otherCasesSlider.addEventListener('touchend', dragStop)
+
+}
+
+function reviewsSliderDrag(){
+    const reviewsSlider = document.querySelector(".reviews-cards-slider")
+
+    let isDragStart = false, prevPageX, prevScrollLeft
+
+    const dragStart = (e) => {
+        isDragStart = true
+        prevPageX = e.pageX || e.touches[0].pageX
+        prevScrollLeft = reviewsSlider.scrollLeft
+        console.log("mousedown")
+    }
+
+    const dragging = (e) => {
+        if(!isDragStart) return
+        e.preventDefault()
+        let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX
+        reviewsSlider.scrollLeft = prevScrollLeft - positionDiff
+        console.log("dragging")
+    }
+
+    const dragStop = () => {
+        isDragStart = false
+        console.log("mouseeup")
+    }
+
+    reviewsSlider.addEventListener('mousedown', dragStart)
+    reviewsSlider.addEventListener('touchstart', dragStart)
+
+    reviewsSlider.addEventListener('mousemove', dragging)
+    reviewsSlider.addEventListener('touchmove', dragging)
+
+    reviewsSlider.addEventListener('mouseup', dragStop)
+    reviewsSlider.addEventListener('touchend', dragStop)
+
+}
+
+otherCasesSliderDrag()
+reviewsSliderDrag()
+otherCasesSliderClick()
+reviewsSliderClick()
