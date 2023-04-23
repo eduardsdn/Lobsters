2f982e1112c8d8a3d0bfdc50fa57047decda548e
eduardsdn <eduard.sdn@gmail.com>
2023-04-23 21:02:26 +0200

    Added desktop login pop-up for webmaster

--- popups.js
+++ popups.js
@@ -1,17 +1,32 @@
 function mobileMenuPopUp(){
     const mobileMenu = document.querySelector(".mobile-menu")
     const mobileMenuButton = document.querySelector(".header-menu-button")
-    const mobileMenuClose = document.querySelector(".close")
+    const mobileMenuClose = document.querySelector("#mobile-menu-close")
     mobileMenuButton.addEventListener('click', function(){
         mobileMenu.classList.remove("hidden")
-        mobileMenu.classList.add("shown")
     })
 
     mobileMenuClose.addEventListener('click', function(){
-        mobileMenu.classList.remove("shown")
         mobileMenu.classList.add("hidden")
     })
 
 }
 
+function loginPopUp(){
+    const loginPopup = document.querySelector(".login-popup")
+    const loginButton = document.querySelector(".log-in-button")
+    const loginClose = document.querySelector("#login-popup-close")
+
+    loginButton.addEventListener('click', function(){
+        loginPopup.classList.remove("hidden")
+        loginPopup.classList.add("shown")
+    })
+
+    loginClose.addEventListener('click', function(){
+        loginPopup.classList.remove("shown")
+        loginPopup.classList.add("hidden")
+    })
+}
+
 mobileMenuPopUp()
+loginPopUp()
