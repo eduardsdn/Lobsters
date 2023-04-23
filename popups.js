e5d3a1c1bacd49f3f71cf41159a466747e52c7ca
eduardsdn <eduard.sdn@gmail.com>
2023-04-24 01:14:47 +0200

    styled mobile pop-ups

--- popups.js
+++ popups.js
@@ -1,4 +1,6 @@
 function mobileMenuPopUp(){
+
+
     const mobileMenu = document.querySelector(".mobile-menu")
     const mobileMenuButton = document.querySelector(".header-menu-button")
     const mobileMenuClose = document.querySelector("#mobile-menu-close")
@@ -10,21 +12,37 @@ function mobileMenuPopUp(){
         mobileMenu.classList.add("hidden")
     })
 
+    
 }
 
 function loginPopUp(){
     const loginPopup = document.querySelector(".login-popup")
     const loginButton = document.querySelector(".log-in-button")
+    const mobileLoginButton = document.querySelector("#mobile-login-button")
     const loginClose = document.querySelector("#login-popup-close")
+    const mobileCloseButton = document.querySelector("#mobile-menu-close")
+    const mobileMenu = document.querySelector(".mobile-menu")
 
     loginButton.addEventListener('click', function(){
         loginPopup.classList.remove("hidden")
         loginPopup.classList.add("shown")
+        mobileMenu.classList.add("hidden")
+    })
+    mobileLoginButton.addEventListener('click', function(){
+        loginPopup.classList.remove("hidden")
+        loginPopup.classList.add("shown")
+        mobileMenu.classList.add("hidden")
     })
 
     loginClose.addEventListener('click', function(){
         loginPopup.classList.remove("shown")
         loginPopup.classList.add("hidden")
+        mobileMenu.classList.add("hidden")
+    })
+    mobileCloseButton.addEventListener('click', function(){
+        loginPopup.classList.remove("hidden")
+        loginPopup.classList.add("shown")
+        mobileMenu.classList.add("hidden")
     })
 }
 
@@ -32,15 +50,32 @@ function registrationPopUp(){
     const registrationPopup = document.querySelector(".registration-popup")
     const registrationButton = document.querySelector(".registration-button")
     const registrationClose = document.querySelector("#registration-popup-close")
+    const mobileRegistrationButton = document.querySelector("#mobile-registration-button")
+    const mobileCloseButton = document.querySelector("#mobile-registration-close")
+    const mobileMenu = document.querySelector(".mobile-menu")
 
     registrationButton.addEventListener('click', function(){
         registrationPopup.classList.remove("hidden")
         registrationPopup.classList.add("shown")
+        mobileMenu.classList.add("hidden")
+    })
+
+    mobileRegistrationButton.addEventListener('click', function(){
+        registrationPopup.classList.remove("hidden")
+        registrationPopup.classList.add("shown")
+        mobileMenu.classList.add("hidden")
     })
 
     registrationClose.addEventListener('click', function(){
         registrationPopup.classList.remove("shown")
         registrationPopup.classList.add("hidden")
+        mobileMenu.classList.add("hidden")
+    })
+
+    mobileCloseButton.addEventListener('click', function(){
+        registrationPopup.classList.remove("shown")
+        registrationPopup.classList.add("hidden")
+        mobileMenu.classList.add("hidden")
     })
 }
 
